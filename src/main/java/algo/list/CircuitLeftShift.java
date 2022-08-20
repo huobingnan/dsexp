ef8a1f203c0ee16a1e67047ef9a3973f1b552cda
manan <75068951+huobingnan@users.noreply.github.com>
2022-08-20 17:44:19 +0800

    顺序表循环左移p位

--- src/main/java/algo/list/CircuitLeftShift.java
+++ src/main/java/algo/list/CircuitLeftShift.java
@@ -7,6 +7,10 @@ import java.util.function.Function;
 /**
  * 将一个顺序表循环左移p位
  *  see also => LeetCode； 2010-408统考（数据结构）； 王道/顺序表/综合分析题
+ *  将顺序表按照循环移位的次数分为AB两个部分 A(0, p-1)，B(p, length-1)
+ *  A' = Reverse(A)
+ *  B' = Reverse(B)
+ *  Reverse(A'B')
  */
 public class CircuitLeftShift implements Function<SequentialList<Integer>, SequentialList<Integer>> {
 
